f8281a3d446fefe233edc6ea7b7392b7b52c1579
unknown <hidri.yosra@gmail.com>
2019-05-21 00:37:23 +0100

    mon premier commit

---
+++ tests/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularKarmaJasminePage } from './app.po';
+
+describe('angular-karma-jasmine App', () => {
+  let page: AngularKarmaJasminePage;
+
+  beforeEach(() => {
+    page = new AngularKarmaJasminePage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to Angular Unit Testing!');
+  });
+});
